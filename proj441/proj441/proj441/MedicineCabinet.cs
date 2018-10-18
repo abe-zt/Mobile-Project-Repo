d9caa9e738ccf4f9cd373a4d3f6cd31173f1b142
abe-zt <zatar002@cougars.csusm.edu>
2018-10-18 01:32:03 -0700

    Update proj441/proj441/proj441/MedicineCabinet.cs
    created medicine cabint

--- proj441/proj441/proj441/MedicineCabinet.cs
+++ proj441/proj441/proj441/MedicineCabinet.cs
@@ -6,5 +6,29 @@ namespace proj441
 {
     class MedicineCabinet
     {
+        public string Name
+        {
+            get;
+            set;
+        }
+        
+        public double Dosage
+        {
+            get;
+            set;
+        }
+
+        public int Count
+        {
+            get;
+            set;
+        }
+
+        public string Notes
+        {
+            get;
+            set;
+        }
+
     }
 }
