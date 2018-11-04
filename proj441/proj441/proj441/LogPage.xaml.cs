62a099ee0bce7c24b9c6c5f5615b5acea8852455
Abel Zatarain Desktop <zatar002@cougars.csusm.edu>
2018-11-04 11:30:55 -0800

    ui changes, integrated resource dictionary
    ui changes,

--- proj441/proj441/proj441/LogPage.xaml.cs
+++ proj441/proj441/proj441/LogPage.xaml.cs
@@ -16,5 +16,11 @@ namespace proj441
 		{
 			InitializeComponent ();
 		}
-	}
+
+        private async void AddPrescriptionButton_Clicked(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            await Navigation.PushAsync(new AddPerscriptionPage());
+        }
+    }
 }
