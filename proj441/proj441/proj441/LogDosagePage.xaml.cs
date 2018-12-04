6c96ec7c13bc58de9906bd02ee65b6cc0a346390
Abel Zatarain Desktop <zatar002@cougars.csusm.edu>
2018-12-04 09:20:49 -0800

    more validation for logging a dose, shouldn't go negative

--- proj441/proj441/proj441/LogDosagePage.xaml.cs
+++ proj441/proj441/proj441/LogDosagePage.xaml.cs
@@ -97,36 +97,44 @@ namespace proj441
 
             //int difference = p2.Remaining - (int)DosageStepper.Value;
             //remainingLabel.Text = difference.ToString();
-            p_copy.Remaining -= (double)DosageStepper.Value;
-            remainingLabel.Text = p_copy.Remaining.ToString();
-
-            //update prescription with correct remaining value
-            await App.MyPrescriptionDatabase.SaveItemAsync(p_copy);
-
-            Dose d1 = new Dose
+            if (p_copy.Remaining - (double)DosageStepper.Value < 0)
             {
-                //should not need to copy over pid, since we want
-                Name = p_copy.Name,
-                ProperName = p_copy.ProperName,
-                Strength = p_copy.Strength,
-                StrengthUnits = p_copy.StrengthUnits,
-                Instructions = p_copy.Instructions,
-                PrescribedDosage = p_copy.PrescribedDosage,
-                PhysicalDescription = p_copy.PhysicalDescription,
-                Quantity = p_copy.Quantity,
-                Remaining = p_copy.Remaining,
-                DateTimeTaken = myDatePicker.Date + myTimePicker.Time,
-                QuantityTaken = (double)DosageStepper.Value
-            };
+                await DisplayAlert("Error:", "Not enough to take dosage!", "OK");
+            }
+            else
+            {
+                p_copy.Remaining -= (double)DosageStepper.Value;
 
-            //d1.PrescriptionTaken.Remaining -= d1.QuantityTaken;
-            await App.MyDoseDatabase.SaveItemAsync(d1);
-            App.MyHistory.Add(d1);
+                remainingLabel.Text = p_copy.Remaining.ToString();
 
-            Analytics.TrackEvent("Successfully added dose");
+                //update prescription with correct remaining value
+                await App.MyPrescriptionDatabase.SaveItemAsync(p_copy);
 
-            await DisplayAlert("Added to History:", "Taken " + " (" + d1.QuantityTaken + ") " + d1.ProperName +" " +d1.Strength + d1.StrengthUnits + " at " + d1.DateTimeTaken.ToString(), "OK");
-            await Navigation.PopAsync();
+                Dose d1 = new Dose
+                {
+                    //should not need to copy over pid, since we want
+                    Name = p_copy.Name,
+                    ProperName = p_copy.ProperName,
+                    Strength = p_copy.Strength,
+                    StrengthUnits = p_copy.StrengthUnits,
+                    Instructions = p_copy.Instructions,
+                    PrescribedDosage = p_copy.PrescribedDosage,
+                    PhysicalDescription = p_copy.PhysicalDescription,
+                    Quantity = p_copy.Quantity,
+                    Remaining = p_copy.Remaining,
+                    DateTimeTaken = myDatePicker.Date + myTimePicker.Time,
+                    QuantityTaken = (double)DosageStepper.Value
+                };
+
+                //d1.PrescriptionTaken.Remaining -= d1.QuantityTaken;
+                await App.MyDoseDatabase.SaveItemAsync(d1);
+                App.MyHistory.Add(d1);
+
+                Analytics.TrackEvent("Successfully added dose");
+
+                await DisplayAlert("Added to History:", "Taken " + " (" + d1.QuantityTaken + ") " + d1.ProperName + " " + d1.Strength + d1.StrengthUnits + " at " + d1.DateTimeTaken.ToString(), "OK");
+                await Navigation.PopAsync();
+            }
         }
 
         private async void CancelToHistory_Clicked(object sender, EventArgs e)
